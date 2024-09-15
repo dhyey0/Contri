0f1d8d54b57a49dda6ea7edc91ccd0adcdb04d5f
dhyey0 <dhyeypatel2907@gmail.com>
2024-09-15 11:00:41 +0530

    Android project

---
+++ app/src/main/java/com/example/contri/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.contri;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        },null,3000);
+    }
+}
